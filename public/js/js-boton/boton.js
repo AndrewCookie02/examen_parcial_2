528b766aa77425577cfa0a45f530633d1dddb720
AndrewCookie02 <andrea.d.chavez.m@gmail.com>
2022-05-23 14:38:10 -0600

    subir

---
+++ public/js/js-boton/boton.js
@@ -0,0 +1,52 @@
+
+var menuVisible = -1;//0= posicion del menu que se visualiza primero. -1 significa ninguna carta visible.
+
+var botonVer = document.querySelectorAll('.BtnVer'); // Inicializando una variable 
+//console.log(botonVer.length);
+
+
+var listacartas = document.querySelectorAll('.listCards');
+
+let seccCarts = document.getElementsByClassName('card');
+
+/*console.log ('listacartas' + listacartas.length);
+
+console.log ('card' + seccCarts.length); */
+
+
+//oculta todas
+listacartas.forEach(function(element) {
+    element.classList.add('novisible');
+  });
+
+
+
+//index = posicion
+botonVer.forEach(function(element,index)
+{
+
+    console.log('posicionlistabotones' + index);
+
+        element.onclick = function () {
+            
+            if (menuVisible >= 0){
+                //diferente !=
+                if (menuVisible != index){
+                    listacartas[menuVisible].classList.remove('visible');
+                    listacartas[menuVisible].classList.add('novisible');
+                } else{
+                    return false; //salir de la funcion 
+                }
+            }
+
+            if(listacartas[index].classList.contains('novisible')){
+                //quitar el visible, y añadir el ocultar
+                listacartas[index].classList.remove('novisible');
+                listacartas[index].classList.add('visible');
+                menuVisible = index;
+            }
+        }
+}
+);
+
+
---
+++ public/js/js-slider/slider.js
@@ -0,0 +1,106 @@
+//cargamos en una variable los elementos que se visualizan en cierto tiempo
+let listaDiv = document.querySelectorAll('.imgslider > div');
+let posVisible = 0;
+
+console.log (listaDiv.length); // Cuantos elementos tiene la lista
+
+//la funcion foreach recorre c/u de los elemntos de la lista
+listaDiv.forEach ((item) => {
+    item.classList.add('oculta');
+} );
+
+    //visualiza el 1 objeto de la lista
+    listaDiv[posVisible].classList.remove('oculta');
+    listaDiv[posVisible].classList.add('visible');
+
+//ejecuta tal funcion despues de tantos milisegundos, pero solo se ejecuta una vez
+
+//cada cierto cierto la sigue ejecutando
+setInterval(() => {    
+    listaDiv.forEach ((item) => {
+        item.classList.add('oculta');
+    } );
+
+    console.log ('elemento visible  ' + posVisible);
+
+     //si el ultimo visible es igual al ultimo elemento, voy a reiniciar el contador 
+     if(posVisible == (listaDiv.length - 1)) {
+        posVisible = 0 //inicia con 0
+    }else {//si en cambio
+        posVisible++;
+        //visualiza la primera, incremento para que en el sig ciclo, se visualice el sig elemento
+    }
+
+    //visualiza el 1 objeto de la lista
+    listaDiv[posVisible].classList.remove('oculta');
+    listaDiv[posVisible].classList.add('visible');
+
+    console.log ('Siguiente a ver' + posVisible)
+
+
+}, 4000);
+
+
+let listacontrols = document.querySelectorAll ('.controls i');
+
+listacontrols.forEach ((item, pos) => {
+    item.onclick = () => {
+
+        posVisible = pos;
+
+        //La funcion forEach recorre cada uno de los elementos de la lista
+        listaDiv.forEach ((item) => {
+            item.classList.add('oculta');
+        } );
+
+        //visualiza el 1 objeto de la lista
+        listaDiv[posVisible].classList.remove('oculta');
+        listaDiv[posVisible].classList.add('visible');
+
+    }
+});
+
+//funcionalidad del boton hacia atras (izq)
+let btnLeft = document.querySelector('.leftright a:nth-child(1)');
+btnLeft.onclick = () =>{
+
+    if (posVisible == 0) {
+        posVisible = listaDiv.length -1;
+    }else{
+        posVisible--;
+    }
+
+    //La funcion forEach recorre cada uno de los elementos de la lista
+    listaDiv.forEach ((item) => {
+        item.classList.add('oculta');
+    } );
+
+    //visualiza el 1 objeto de la lista
+    listaDiv[posVisible].classList.remove('oculta');
+    listaDiv[posVisible].classList.add('visible');  
+
+    console.log ('Left ' + posVisible);
+}
+
+
+//funcionalidad del boton hacia adelante (derecho)
+let btnRight = document.querySelector('.leftright a:nth-child(2)');
+
+btnRight.onclick = () => {
+
+    //La funcion forEach recorre cada uno de los elementos de la lista
+    listaDiv.forEach ((item) => {
+        item.classList.add('oculta');
+    } );
+    
+    if (posVisible == (listaDiv.length - 1)) {
+        posVisible = 0;
+    }else {
+        posVisible++; //Incremento para que en el siguiente ciclo, se visualice el sigueinte elemento
+    }
+
+    //visualiza el 1 objeto de la lista
+    listaDiv[posVisible].classList.remove('oculta');
+    listaDiv[posVisible].classList.add('visible');
+
+}
