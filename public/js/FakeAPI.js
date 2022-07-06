be18d629fd7f8a5e05e78da6ba722ac06a7d76c3
AndrewCookie02 <andrea.d.chavez.m@gmail.com>
2022-07-06 00:14:21 -0600

    subir

--- public/js/FakeAPI.js
+++ public/js/FakeAPI.js
@@ -11,7 +11,7 @@ fetch(pUrl)
         let boton = document.createElement ('button');
        // console.log(card[index]);
         
-        boton.setAttribute("type","button"); //noimbre del atributo y su valor.
+        boton.setAttribute("type","button"); //nombre del atributo y su valor.
         boton.setAttribute("class" ,"btn btn-primary verModal");
         boton.setAttribute("data-bs-target", "#Modal");
         boton.setAttribute("data-bs-toggle", "modal");
@@ -21,7 +21,17 @@ fetch(pUrl)
         //funcion onlick
         boton.onclick = () =>{
             let fila = "<tr> <td> Sugar </th> <th>"+ element.Sugar + "</td>  </tr>" +
-            "<tr> <td> Sugar </th> <th>"+ element.Eggs + "</td>  </tr>";
+            "<tr> <td> Sugar </th> <th>"+ element.Eggs + "</td>  </tr>" +
+            "<tr> <td> Baking Powder </th> <th>"+ element.BakingPowder + "</td>  </tr>" + 
+            "<tr> <td> Margarine or softened butter </th> <th>"+ element.Margarineorsoftenedbutter + "</td>  </tr>" +
+            "<tr> <td> Flour </th> <th>"+ element.Flour + "</td>  </tr>" +
+            "<tr> <td> Vanilla extract </th> <th>"+ element.Vanillaextract + "</td>  </tr>" +
+            "<tr> <td> Salt </th> <th>"+ element.Salt +
+            +
+            "<tr> <td> CakeMix </th> <th>"+ element.Mix + "</td>  </tr>" +
+            "<tr> <td> Fruits </th> <th>"+ element.Fruits + "</td>  </tr>"
+            ;
+
 
             document.querySelector(".tableMenu tbody").innerHTML = fila;
         }
@@ -33,7 +43,7 @@ fetch(pUrl)
    
 /*
     let tbody = "<tbody>" ; 
-    let lista = data.Ingredientes;
+    let lista = data.Ingredients;
     
     */
 })
